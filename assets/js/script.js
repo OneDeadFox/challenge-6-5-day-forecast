816f2d7df9febf860eac077ba71909e1f075fb39
OneDeadFox <tjosephb.pro@gmail.com>
2023-01-08 14:59:16 -0800

    finished 5day forecast set up and display

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,20 +9,18 @@
 $ (function (){
     let searchButton = $('#search-btn');
     let searchCrit = $('#search-criteria');
-    let weather = {
-      city: '',
-      date: '', //unix result
-      weatherType: '',
-      temp: '', //kelvin result
-      humidity: '', //add %
-      windSpeed: '', //round up add mph
-    };
-    let forecast = [];
+    let weatherBanner = $('#weather-banner');
+    let forecastDays = $('#forecast-days')
+    let weatherIcon;
+    let input;
 
+  initialSearch();
 
 //Event Listeners------------------------------------------------
     searchButton.on('click', function(){
-        fetchForecast();
+      //user input  
+      input = '&location=' + searchCrit.val();  
+      fetchForecast();
     });
 
     searchCrit.keyup(function(e){
@@ -34,10 +32,12 @@ $ (function (){
     });
 
 //Functions------------------------------------------------------
-    function fetchForecast(){
-      //user input  
-      var input = '&location=' + searchCrit.val();
+    function initialSearch(){
+      input = "&location=Seattle";
+      fetchForecast();
+    }
 
+    function fetchForecast(){
       //position vars
       var posAccessKey = 
       '?key=ZFD5pPPWBtt5h0OFfjA2eC0JOkZpOdkd'
@@ -54,6 +54,17 @@ $ (function (){
       'https://api.openweathermap.org/data/2.5/weather?'
       var requestForecast = 
       'https://api.openweathermap.org/data/2.5/forecast?'
+
+      //response receivers objects
+      var weather = {
+        city: '',
+        date: '', //unix result
+        weatherType: '',
+        temp: '', //kelvin result
+        humidity: '', //add %
+        windSpeed: '', //round up add mph
+      };
+      var fullForecast = [];
 
       
       //lat and lon request
@@ -79,9 +90,11 @@ $ (function (){
               return res.json();
             })
               .then(function(data){
-                weather.city = data.name;
-                weather.date = dayjs.unix(data.dt).format('ddd, MMM DD, YYYY');
+                console.log(data);
+                weather.city = data.name + ", " + data.sys.country;
+                weather.date = dayjs.unix(data.dt).format('MMM DD, YYYY');
                 weather.weatherType = data.weather[0].main;
+                weather.icon = data.weather[0].icon;
                 weather.temp = Math.round(1.8*(data.main.temp - 273) + 32);
                 weather.humidity = data.main.humidity;
                 weather.windSpeed = Math.round(data.wind.speed);
@@ -96,11 +109,13 @@ $ (function (){
                 var dayCount = 0;
                 var currentDay;
                 var _test = 0;
+
                 //5 day forecast objects
                 //#region Day Objects
                 var day0 = {
                   date: '',
                   weather: [],
+                  icon: [],
                   temp: [],
                   humidity: [],
                   windSpeed: []
@@ -108,6 +123,7 @@ $ (function (){
                 var day1 = {
                   date: '',
                   weather: [],
+                  icon: [],
                   temp: [],
                   humidity: [],
                   windSpeed: []
@@ -115,6 +131,7 @@ $ (function (){
                 var day2 = {
                   date: '',
                   weather: [],
+                  icon: [],
                   temp: [],
                   humidity: [],
                   windSpeed: []
@@ -122,6 +139,7 @@ $ (function (){
                 var day3 = {
                   date: '',
                   weather: [],
+                  icon: [],
                   temp: [],
                   humidity: [],
                   windSpeed: []
@@ -129,6 +147,7 @@ $ (function (){
                 var day4 = {
                   date: '',
                   weather: [],
+                  icon: [],
                   temp: [],
                   humidity: [],
                   windSpeed: []
@@ -136,7 +155,9 @@ $ (function (){
                 //#endregion Day Objects
 
                 //array to hold 5 day forecast objects
-                var fullForecast = [day0, day1, day2, day3, day4];
+                fullForecast = [day0, day1, day2, day3, day4];
+
+                //set 5 day forecast object values from response-
                 for (let i = 0; i < data.list.length; i++) {
                   //set current day to day in index of response array
                   currentDay = dayjs.unix(data.list[i].dt).format('MM/DD/YYYY');
@@ -147,6 +168,7 @@ $ (function (){
                   else if(fullForecast[0].date == ''){
                     fullForecast[dayCount].date = currentDay;
                     fullForecast[dayCount].weather.push(data.list[i].weather[0].main);
+                    fullForecast[dayCount].icon.push(data.list[i].weather[0].icon);
                     fullForecast[dayCount].temp.push(data.list[i].main.temp);
                     fullForecast[dayCount].humidity.push(data.list[i].main.humidity);
                     fullForecast[dayCount].windSpeed.push(data.list[i].wind.speed);
@@ -155,6 +177,7 @@ $ (function (){
                   else if(currentDay === fullForecast[dayCount].date){
                     fullForecast[dayCount].date = currentDay;
                     fullForecast[dayCount].weather.push(data.list[i].weather[0].main);
+                    fullForecast[dayCount].icon.push(data.list[i].weather[0].icon);
                     fullForecast[dayCount].temp.push(data.list[i].main.temp);
                     fullForecast[dayCount].humidity.push(data.list[i].main.humidity);
                     fullForecast[dayCount].windSpeed.push(data.list[i].wind.speed);
@@ -164,15 +187,14 @@ $ (function (){
                     dayCount++;
                     fullForecast[dayCount].date = currentDay;
                     fullForecast[dayCount].weather.push(data.list[i].weather[0].main);
+                    fullForecast[dayCount].icon.push(data.list[i].weather[0].icon);
                     fullForecast[dayCount].temp.push(data.list[i].main.temp);
                     fullForecast[dayCount].humidity.push(data.list[i].main.humidity);
                     fullForecast[dayCount].windSpeed.push(data.list[i].wind.speed);
                   }
                 }
-                console.log(fullForecast);
-                console.log(data)
 
-                //consolidate fullforecast arrays
+                //consolidate fullforecast arrays----------------
                 for (let i = 0; i < fullForecast.length; i++) {
                   var day = fullForecast[i];
                   
@@ -181,8 +203,84 @@ $ (function (){
                       day.weather.filter(val => val === a).length 
                       - day.weather.filter(val => val === b).length
                       ).pop();
+                  day.icon = day.icon.sort((a,b) => 
+                      day.icon.filter(val => val === a).length 
+                      - day.icon.filter(val => val === b).length
+                      ).pop();
+                  day.temp = Math.round(1.8*((day.temp.reduce((a, b) => a + b) / day.temp.length) - 273) + 32);
+                  day.humidity = Math.round(day.humidity.reduce((a, b) => a + b) / day.humidity.length);
+                  day.windSpeed = Math.round(day.windSpeed.reduce((a, b) => a + b) / day.windSpeed.length);
+                  
+
+                  console.log(day.temp);
+                  console.log(day.humidity);
+                  console.log(day.windSpeed);
+                  console.log(day.icon);
+
                 }
+                setWeatherBannerElement(weather);
+                setForecastElements(fullForecast);
               });
           });
     }
+
+    function emojiSetter(el){
+
+    }
+
+    function setWeatherBannerElement(el){
+      weatherIcon = $('<img id="banner-img">');
+      weatherIcon.attr('src', `http://openweathermap.org/img/wn/${el.icon}@2x.png`)
+
+      //set banner heading
+      weatherBanner.children('#city-date-weather').text(el.city + ' - ' + el.date + ' ' + el.weatherType);
+      weatherBanner.children('#city-date-weather').append(weatherIcon)
+      console.log(weatherIcon);
+
+      //set banner details
+      weatherBanner.children('#banner-temp').text('Temp: ' + el.temp + '℉');
+      weatherBanner.children('#banner-humidity').text('Humidity: ' + el.humidity + '%');
+      weatherBanner.children('#banner-wind').text('Wind: ' + el.windSpeed + 'MPH');
+    }
+
+    function setForecastElements(arr){
+      $('.card').remove();
+      var forecastDays = $('#forecast-days')
+
+      //set card items
+      for (let i = 0; i < arr.length; i++) {
+        //set card icon
+        var card = $('<div class="col-2 card">');
+        var cardIcon = $(`<img id="card-icon-${i}">`);
+        cardIcon.attr('src', `http://openweathermap.org/img/wn/${arr[i].icon}@2x.png`);
+        cardIcon.attr('class', 'card-img');
+        
+        //set card values
+        card.attr('id', `day${i}`);
+        card.attr('class', 'col-2 card');
+        card.append(`<p id="card${i}-date" class="card-item bold">`);
+        card.children(`#card${i}-date`).text(arr[i].date);
+
+        //set weather
+        card.append(`<p id="card${i}-weather" class="card-item">`);
+        card.children(`#card${i}-weather`).text(arr[i].weather + ' ');
+        card.children(`#card${i}-weather`).append(cardIcon);
+
+        //set temp
+        card.append(`<p id="card${i}-temp" class="card-item">`);
+        card.children(`#card${i}-temp`).text('Temp: ' + arr[i].temp + '℉');
+
+        //set humidity
+        card.append(`<p id="card${i}-humidity" class="card-item">`);
+        card.children(`#card${i}-humidity`).text('Humidity: ' + arr[i].humidity + '%');
+
+        //set wind
+        card.append(`<p id="card${i}-wind" class="card-item">`);
+        card.children(`#card${i}-wind`).text('Wind: ' + arr[i].windSpeed + 'MPH');
+        
+        forecastDays.append(card);
+      }
+    }
+
+    function setSearchHistory(){}
 });
